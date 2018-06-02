c652090f77f8c5a72a5adfe1ea204a13255103fb
Santosh Ganapa <Santosh.Ganapa@Mvtechlabs.com>
2018-06-02 19:44:34 +0530

    Added reduce function scenario and reduceLeft and reduceRight

--- src/main/scala/sbtexample/Main.scala
+++ src/main/scala/sbtexample/Main.scala
@@ -17,5 +17,33 @@ object Main extends App {
   println(s" Sum of all numbers using reduceLeft:${a.reduceLeft((x,y) => x + y)}")
   println(s" Multiplication using reduceLeft:${a.reduceLeft(_ * _)}")
   println(s" Min values from list using reduceLeft:${a.reduceLeft(_ min _)}")
-  println(s" Min values from list using reduceLeft:${a.reduceLeft(_ max _)}")
+  println(s" Max values from list using reduceLeft:${a.reduceLeft(_ max _)}")
+
+
+  // returns the max of the two elements
+  val findMax = (x: Int, y: Int) => {
+    val winner = x max y
+    println(s"compared $x to $y, $winner was larger")
+    winner
+  }
+
+  //Now call reduceLeft again on the array, this time giving it the findMax function:
+  println(s" Min values from list using findMax and reduceLeft:${a.reduceLeft(_ max _)}")
+
+  // Find longest & shortest string from list of strings
+  val names = Vector("Om", "Sai", "Ganesh", "Ram", "Yamaha")
+  println(s"Names:$names")
+  val longestString = names.reduceLeft((x,y) => if(x.length > y.length) x else y)
+  val shortestString = names.reduceLeft((x,y) => if(x.length < y.length) x else y)
+
+  println(s"LongestString:$longestString  ShortestString:$shortestString")
+
+  /*****
+    * The difference between reduceLeft and reduceRight
+  In many algorithms, it may not matter if you call reduceLeft or reduceRight.
+  In that case, you can call reduce instead. The reduce Scaladoc states, “The order in which operations
+  are performed on elements is unspecified and may be nondeterministic.”
+    ******/
+  println(s"LongestString using reduce: ${names.reduce((x,y)=> if(x.length>y.length) x else y)}")
+  println(s"ShortestString using reduce: ${names.reduce((x,y)=> if(x.length<y.length) x else y)}")
 }
